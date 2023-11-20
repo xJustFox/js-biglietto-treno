db2afe5851f5e8e6d14bf013878e3df4e78b1a48
xJustFox <dodydaversa@gmail.com>
2023-11-20 17:01:20 +0100

    [FIx] Fixed discount calc

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ let price_ticket = distance_km * 0.21;
 
 // Sè l'utente avrà meno di 18 anni verra applicato il 20% di sconto
 if (age < 18) {
-    price_ticket = (price_ticket * 20) / 100;
+    discount = (price_ticket * 20) / 100;
+    price_ticket -= discount
 } 
 // Sè l'utente avrà più di 18 anni ma meno di 65 verra applicato il 40% di sconto
 else if (age >= 18 && age < 65) {
-    price_ticket = (price_ticket * 40) / 100;
+    discount = (price_ticket * 40) / 100;
+    price_ticket -= discount
 }
 
 console.log(price_ticket.toFixed(2));
