95dd1855c426fbfb191a1c9b7f349874fedaa64a
xJustFox <dodydaversa@gmail.com>
2023-11-20 16:13:09 +0100

    [Upgrade] Initialized and defined the variables

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Variabile distanza km da percorrere
+let distance_km = prompt("Quanti km dovrai percorrere?");
+
+// Variabile età dell'utente
+let age = prompt("Quanti anni hai?");
+
+// Variabile prezzo del biglietto
+let price_ticket = distance_km * 0.21;
